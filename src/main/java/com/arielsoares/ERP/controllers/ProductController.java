b0e33cbddf3c90f1e34e0bea83150787f1e0b5f7
Ariel Soares <ariel.sfranco@gmail.com>
2025-01-15 11:12:56 -0300

    Product creation added

--- src/main/java/com/arielsoares/ERP/controllers/ProductController.java
+++ src/main/java/com/arielsoares/ERP/controllers/ProductController.java
@@ -1,18 +1,35 @@
 package com.arielsoares.ERP.controllers;
 
 import com.arielsoares.ERP.entities.Product;
+import com.arielsoares.ERP.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/products")
 public class ProductController {
 
+    @Autowired
+    private ProductService service;
+
     @GetMapping
-    public ResponseEntity<Product> getProducts() {
-        return ResponseEntity.ok(new Product(1L, "cadeira", "cadeira para sentar", 25.35));
+    public ResponseEntity<List<Product>> findAll(){
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @PostMapping
+    public ResponseEntity<Product> insert(@RequestBody Product product){
+        Product obj = service.insert(product);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+
+
 
 }
---
+++ src/main/java/com/arielsoares/ERP/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.arielsoares.ERP.repositories;
+
+import com.arielsoares.ERP.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}

32f73354d480564fce4b6ed1fb72dc5ad814c9d2
Ariel Soares <ariel.sfranco@gmail.com>
2025-01-15 12:06:26 -0300

    Product Service and controller refactored

--- src/main/java/com/arielsoares/ERP/controllers/ProductController.java
+++ src/main/java/com/arielsoares/ERP/controllers/ProductController.java
@@ -23,6 +23,12 @@ public class ProductController {
         return ResponseEntity.ok().body(list);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id){
+        Product obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
     @PostMapping
     public ResponseEntity<Product> insert(@RequestBody Product product){
         Product obj = service.insert(product);
@@ -30,6 +36,10 @@ public class ProductController {
         return ResponseEntity.created(uri).body(obj);
     }
 
-
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> update(@RequestBody Product product, @PathVariable Long id){
+        product = service.update(id,product);
+        return ResponseEntity.ok().body(product);
+    }
 
 }
--- src/main/java/com/arielsoares/ERP/services/ProductService.java
+++ src/main/java/com/arielsoares/ERP/services/ProductService.java
@@ -1,11 +1,15 @@
 package com.arielsoares.ERP.services;
 
 import com.arielsoares.ERP.entities.Product;
+import com.arielsoares.ERP.exceptions.ResourceNotFoundException;
 import com.arielsoares.ERP.repositories.ProductRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
@@ -17,8 +21,30 @@ public class ProductService {
         return repository.findAll();
     }
 
+    public Product findById(Long id){
+        Optional<Product> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    @Transactional
     public Product insert(Product product){
         return repository.save(product);
     }
 
+    @Transactional
+    public Product update(Long id, Product obj){
+        try {
+            Product product = findById(id);
+            updateData(product, obj);
+            return repository.save(product);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Product product, Product obj) {
+        product.setName(obj.getName());
+        product.setDescription(obj.getDescription());
+        product.setPrice(obj.getPrice());
+    }
 }
